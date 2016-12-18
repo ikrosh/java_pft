ee5f768a61f46d073e6ea7e3841c8ef2956980cb
ikrosh <ikroshkina@gmail.com>
2016-12-19 01:32:28 +0200

    Домашнее задание 2 с конструктором

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,28 +4,15 @@ public class Point {
 
   public double x;
   public double y;
-  
 
-  //Сделать запускаемый класс, то есть содержащий функцию
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
 
-  public static void main(String[] args) {
-
-    Point p1 = new Point();
-    p1.x = 2;
-    p1.y = 4;
-
-    Point p2 = new Point();
-    p2.x = 5;
-    p2.y = 8;
-
-    System.out.println("Расстояние между точками " + distance(p1, p2));
   }
-
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
-
 
 }
+
+
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -3,10 +3,18 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world! New Year is coming!");
+    //System.out.println("Hello, world! New Year is coming!");
 
+    Point p1 = new Point (2,4);
+    Point p2 = new Point(5,8);
+    System.out.println("Расстояние между точками " + distance(p1, p2));
 
   }
+
+  public static double distance (Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
+
 
 }
 
