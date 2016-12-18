e22d663e230df2f134a1621c7eb424d3e38b069d
ikrosh <ikroshkina@gmail.com>
2016-12-19 01:51:10 +0200

    Домашнее задание 2 - задача 4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -7,13 +7,10 @@ public class MyFirstProgram {
 
     Point p1 = new Point (2,4);
     Point p2 = new Point(5,8);
-    System.out.println("Расстояние между точками " + distance(p1, p2));
+    System.out.println("Расстояние между точками " + p1.distance(p2));
 
   }
 
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
 
 
 }
