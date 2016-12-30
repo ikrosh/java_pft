29e95599b2758a6556652709d95e24ba4719f6bf
ikrosh <ikroshkina@gmail.com>
2016-12-30 21:49:33 +0200

    Домашнее задание 6

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification () {
+    app.getNavigationHelper().goToContactPage();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm (new ContactData("test1", "test2", "test4", "test@gmail.com", "1112223334455", "test3"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().goToContactPage();
+
+  }
+
+}
