7ba2399e5afb3874e7adcf17f63212f0cedbb87c
ikrosh <ikroshkina@gmail.com>
2017-01-11 04:58:00 +0200

    Упражнение 7 - добавлен запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
@@ -18,7 +19,7 @@ public class TestBase {
     app.stop();
   }
 
-  public ApplicationManager getApp() {
+  /*public ApplicationManager getApp() {
     return app;
-  }
+  }*/
 }
