4e6c5b214deeb73750e08f62754059b7413e1b45
ikrosh <ikroshkina@gmail.com>
2017-01-23 00:25:14 +0200

    Добавлен счетчик при создании группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -56,6 +56,10 @@ public class GroupHelper extends BaseHelper {
     return isElementPresent(By.name("selected[]"));
   }
 
+  public int getGroupCount() {
+    return wd.findElements(By.name("selected[]")).size();
+  }
+
 }
 
 
