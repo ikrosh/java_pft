8fe0ca36baee554a1dce565e17584b8a4d98db85
ikrosh <ikroshkina@gmail.com>
2017-02-24 00:45:39 +0200

    Добавлено сравнение списков создания группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -14,8 +14,23 @@ public class GroupData {
     return id;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
   public GroupData(String header, String name, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.header = header;
     this.name = name;
     this.footer = footer;
@@ -38,24 +53,6 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != groupData.id) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
   }
 
   @Override
