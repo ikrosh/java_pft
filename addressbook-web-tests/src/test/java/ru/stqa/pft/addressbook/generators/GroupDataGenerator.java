f634b45f1854f9e08a9c26aa8aa5b247c742c3b0
ikrosh <ikroshkina@gmail.com>
2017-02-28 01:23:28 +0200

    Revert "для отката"
    This reverts commit 4eddee438755fc1378aebed722124fdae67f906d.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.generators;
 
+import ru.stqa.pft.addressbook.model.GroupData;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
-import ru.stqa.pft.addressbook.model.GroupData;
+
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,7 +15,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class GroupDataGenerator {
 
@@ -32,33 +32,36 @@ public class GroupDataGenerator {
     JCommander jCommander = new JCommander(generator);
     try {
       jCommander.parse(args);
-    }catch(ParameterException ex){
+    }
+    catch (ParameterException ex) {
       jCommander.usage();
       return;
     }
     generator.run();
-
   }
 
   private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    if (format.equals("csv")){
-      saveAsCsv(groups, new File (file));
-    }else if (format.equals("xml")){
-      saveAsXml(groups, new File (file));
-    }else if (format.equals("json")){
-      saveAsJson(groups, new File (file));
+    if (format.equals("csv")) {
+      saveAsCsv(groups, new File(file));
     }
-    else{
+    else if (format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    }
+    else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
+    }
+    else {
       System.out.println("Unrecognized format" + format);
     }
   }
 
-  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
-    Gson gson = new GsonBuilder(). setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-    String json = gson.toJson(groups);
-    try (Writer writer = new FileWriter(file)){
-      writer.write(json);
+  private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    try (Writer writer = new FileWriter(file)) {
+      for (GroupData group : groups) {
+        writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+      }
     }
   }
 
@@ -66,29 +69,25 @@ public class GroupDataGenerator {
     XStream xstream = new XStream();
     xstream.processAnnotations(GroupData.class);
     String xml = xstream.toXML(groups);
-    try (Writer writer = new FileWriter(file)){
+    try (Writer writer = new FileWriter(file)) {
       writer.write(xml);
     }
   }
 
-  private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
-    System.out.println(new File(".").getAbsolutePath());
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
     try (Writer writer = new FileWriter(file)) {
-      for (GroupData group: groups){
-        writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
-      }
+      writer.write(json);
     }
   }
 
   private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
-    for (int i = 0; i < count; i++){
-      groups.add(new GroupData()
-              .withName(String.format("test %s",i))
-              .withHeader(String.format("testheader %s",i))
-              .withFooter(String.format("testfooter %s",i)));
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i))
+              .withFooter(String.format("footer %s", i)));
     }
     return groups;
   }
-
 }
